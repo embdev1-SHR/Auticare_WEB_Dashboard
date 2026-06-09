de854c9e717cb8396fc86c14d238831e622eccf5
Sreejith.R <shrsree341o@gmail.com>
2026-06-09 20:57:38 +0530

    Update alert.js

--- components/shared/alert.js
+++ components/shared/alert.js
@@ -1,20 +1,28 @@
-//SweetAlert
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
 import SweetAlert from "react-bootstrap-sweetalert";
 
 const Alert = ({ onHandleConfirm, onDelete }) => {
+  const [mounted, setMounted] = useState(false);
 
-  return (
-    <>
-      <SweetAlert
-        title='Are you sure?'
-        warning
-        showCancel
-        confirmBtnBsStyle='success'
-        cancelBtnBsStyle='danger'
-        onConfirm={onHandleConfirm}
-        confirmBtnText='Yes, delete!'
-        onCancel={onDelete} />
-    </>
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return createPortal(
+    <SweetAlert
+      title='Are you sure?'
+      warning
+      showCancel
+      confirmBtnBsStyle='success'
+      cancelBtnBsStyle='danger'
+      onConfirm={onHandleConfirm}
+      confirmBtnText='Yes, delete!'
+      onCancel={onDelete}
+    />,
+    document.body
   );
 };
 
